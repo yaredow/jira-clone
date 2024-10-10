4fee2b9e77fc4ae1b1596d84860b262cec23914f
yaredow <yaredyilma11@gmail.com>
2024-10-10 21:17:14 +0300

    finish configuring the button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Jira Clone</div>;
+  return (
+    <div className=" space-x-4">
+      <Button variant="default">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Link</Button>
+      <Button variant="teritery">Teritery</Button>
+    </div>
+  );
 }
