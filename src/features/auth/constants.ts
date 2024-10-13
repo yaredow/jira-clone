aaccfb44cc9584be37f099e2f0ac3f876edd3091
yaredow <yaredyilma11@gmail.com>
2024-10-13 16:20:57 +0300

    finish the user creation functionality

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "yada-jira-clone-auth-cookie";
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,17 +1,34 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { SigninSchema, SignupSchema } from "@/lib/validators";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
-  .post("/login", zValidator("json", SigninSchema), (c) => {
+  .post("/login", zValidator("json", SigninSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
     return c.json({ email, password });
   })
-  .post("/register", zValidator("json", SignupSchema), (c) => {
+  .post("/register", zValidator("json", SignupSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
 
-    return c.json({ name, email, password });
+    const { account } = await createAdminClient();
+    const user = await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ data: user });
   });
 
 export default app;
