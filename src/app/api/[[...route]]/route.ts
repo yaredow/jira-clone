673256c39a1625de183299007aed3765bc91cd12
yaredow <yaredyilma11@gmail.com>
2024-10-18 09:56:55 +0300

    finish configuring the members route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,12 +2,14 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import authRoute from "@/features/auth/server/route";
 import workspacesRoute from "@/features/workspaces/server/route";
+import membersRoute from "@/features/members/server/route";
 
 const app = new Hono().basePath("/api");
 
 const routes = app
   .route("/auth", authRoute)
-  .route("/workspaces", workspacesRoute);
+  .route("/workspaces", workspacesRoute)
+  .route("/members", membersRoute);
 
 export const GET = handle(app);
 export const POST = handle(app);
